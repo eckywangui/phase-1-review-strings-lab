62b584701c0c3a151f60ef477a69340fe97e8ff0
Eckra Wangui <+>
2023-09-19 15:01:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Eckra';
+
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage= `Welcome to Flatbook, ${currentUser}!`.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
